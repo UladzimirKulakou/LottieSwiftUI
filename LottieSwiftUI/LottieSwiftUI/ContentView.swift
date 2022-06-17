5bec2d5a79ae1b3216fda6e1f5f633ae2a27a0c5
Kulakoff <86190227+UladzimirKulakou@users.noreply.github.com>
2022-06-17 21:01:16 +0300

    commit 5
    animation work

--- LottieSwiftUI/LottieSwiftUI/ContentView.swift
+++ LottieSwiftUI/LottieSwiftUI/ContentView.swift
@@ -7,19 +7,59 @@
 
 import SwiftUI
 import CoreData
+import Lottie
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, ").padding()
+struct ContentView: UIViewRepresentable {
+    let name = "90366-the-player"
+    let loopMode: LottieLoopMode = .loop
     
     
-    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+
+        let animationView = AnimationView()
+
+        
+        let animation = Animation.named(name)
+        animationView.loopMode = loopMode
+        animationView.animation = animation
+        animationView.play()
+        animationView.contentMode = .scaleAspectFill
+        
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        return view
+        
     }
+    func updateUIView(_ uiView: UIView, context: Context) {
 
+    }
+    
+
+}
+struct ContentViewNavigation: View {
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(
+                    destination: LottieView()) {
+                    ContentView()
+                }
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        //ContentView()
+        ContentViewNavigation()
     }
 }
--- LottieSwiftUI/LottieSwiftUI/LottieSwiftUIApp.swift
+++ LottieSwiftUI/LottieSwiftUI/LottieSwiftUIApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct LottieSwiftUIApp: App {
     let persistenceController = PersistenceController.shared
+    
+    
 
     var body: some Scene {
         WindowGroup {
