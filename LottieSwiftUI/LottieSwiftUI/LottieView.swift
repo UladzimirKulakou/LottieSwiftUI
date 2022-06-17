3cce2c09abcb37b4c7ebffd8513a4159ff608950
Kulakoff <86190227+UladzimirKulakou@users.noreply.github.com>
2022-06-17 20:05:09 +0300

    commit 2
    add lottie swiftPackege

---
+++ LottieSwiftUI/LottieSwiftUI/LottieView.swift
@@ -0,0 +1,20 @@
+//
+//  LottieView.swift
+//  LottieSwiftUI
+//
+//  Created by Владимир  on 17.06.22.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView()
+    }
+}
