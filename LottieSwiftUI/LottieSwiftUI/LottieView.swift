373ed7df7cd15a14af66af033f28a5bdc52afa6a
Kulakoff <86190227+UladzimirKulakou@users.noreply.github.com>
2022-06-17 20:26:36 +0300

    commit 3
    add constraint - they doesn't work fck

--- LottieSwiftUI/LottieSwiftUI/LottieView.swift
+++ LottieSwiftUI/LottieSwiftUI/LottieView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import Lottie
 
-struct LottieView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LottieView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+
+        let animationView = AnimationView()
+
+        
+        let animation = Animation.named("106649-learn")
+        animationView.animation = animation
+        animationView.play()
+        animationView.contentMode = .scaleAspectFill
+        
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        return view
     }
+
+    func updateUIView(_ uiView: UIView, context: Context) {
+
+    }
+
 }
 
 struct LottieView_Previews: PreviewProvider {
